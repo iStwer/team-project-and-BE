c20916e346f1c5505669ffd4949aa0dff5b3ed52
ivizur <ivana.zurkova45@gmail.com>
2024-06-23 17:41:49 +0200

    Created Header component

---
+++ src/pages/header/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+    return <div>
+        <h1 id="logo">PhysioReact</h1>
+        <ul>
+            <li>Naše služby</li>
+            <li>O nás</li>
+            <li>Ceník</li>
+            <li>Kontakty</li>
+        </ul>
+        <button>Objednat se</button>
+    </div>
+}
