ec74f2c3ce1f96fe71468844825100d17ffbc169
Helena Beranová <helca.beranova@gmail.com>
2024-06-25 20:12:11 +0200

    contact form created

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,14 +1,65 @@
 import { Container } from 'react-bootstrap';
+import { useState, ChangeEvent, FormEvent } from 'react';
+
+interface FormInputData {
+  name: string;
+  email: string;
+  message: string;
+}
 
 export const Contact = () => {
+  const [inputData, setInputData] = useState<FormInputData>({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInputData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(inputData);
+  };
+
   return (
     <Container>
-      <h1>Kontakt</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-        tempor, metus id tristique interdum, lectus sem tristique enim, non
-        ornare urna dui eu risus. Donec ut porta purus, eget lobortis nibh.
-      </p>
+      <h1>Napište nám</h1>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type='text'
+              name='name'
+              placeholder='vaše jméno'
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              name='email'
+              placeholder='váš e-mail'
+              onChange={handleChange}
+            />
+          </div>
+          <input
+            type='text'
+            id='your-message'
+            name='message'
+            placeholder='vaše zpráva'
+            onChange={handleChange}
+          />
+          <button type='submit'>Odeslat</button>
+        </form>
+        <div>
+          <h4>Potřebujete poradit?</h4>
+          <p>stačí nám zavolat na linku: 158</p>
+        </div>
+      </section>
     </Container>
   );
 };
