f642480350192d2bcbd469910527d06140b94fd3
Helena Beranová <helca.beranova@gmail.com>
2024-06-25 12:23:06 +0200

    Prices page content

--- src/pages/prices/Prices.tsx
+++ src/pages/prices/Prices.tsx
@@ -1,10 +1,34 @@
+import { Link } from 'react-router-dom';
+
 export const Prices = () => {
-    return (
+  return (
+    <div>
+      <h1>Ceník</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>Vstupní vyšetření</td>
+            <td>1 000 Kč</td>
+          </tr>
+          <tr>
+            <td>Kontrolní vyšetření - 30 minut</td>
+            <td>590 Kč</td>
+          </tr>
+          <tr>
+            <td>Kontrolní vyšetření - 45 minut</td>
+            <td>890 Kč</td>
+          </tr>
+        </tbody>
+
         <div>
-            <h1>Ceník</h1>
-            <p>
-                50, 100
-            </p>
+          <p>
+            Chcete se poradit ohledně vašeho problému, nebo se nechat objednat?
+          </p>
+          <Link to='/bookingform'>
+            <button>Objednat se online</button>
+          </Link>
         </div>
-    )
+      </table>
+    </div>
+  );
 };
