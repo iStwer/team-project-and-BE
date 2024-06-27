03f9b23d3bc9391727ce42b4f530fe7a0a6c4ed3
Helena Beranová <helca.beranova@gmail.com>
2024-06-27 20:07:51 +0200

    merge conflict resolve

--- src/pages/about-us/EmployeeCard.tsx
+++ src/pages/about-us/EmployeeCard.tsx
@@ -1,26 +1,24 @@
 import { Container } from 'react-bootstrap';
-import { employees } from './Employees';
 
-interface Employees {
+interface Employee {
   id: number;
   name: string;
   specialization: string;
   education: string;
   freeTime: string;
   others: string;
-  src: string;
   alt: string;
-};
+}
 
-export const EmployeeCard = ({ employees }: { employees: Employees }) => {
+export const EmployeeCard = ({ employee }: { employee: Employee }) => {
   return (
-      <Container>
-          <h4 className='accordion-name'>{employees.name}</h4>
-          <p>Specializace: {employees.specialization}</p>
-          <p>Vzdelání: {employees.education}</p>
-          <p>Volný čas: {employees.freeTime}</p>
-          <p>{employees.others}</p>
-          <img src={employees.src} alt={employees.alt}></img>
-      </Container>
+    <Container>
+      <h4 className='accordion-name'>{employee.name}</h4>
+      <p>Specializace: {employee.specialization}</p>
+      <p>Vzdelání: {employee.education}</p>
+      <p>Volný čas: {employee.freeTime}</p>
+      <p>{employee.others}</p>
+      <img src='' alt={employee.alt}></img>
+    </Container>
   );
 };
