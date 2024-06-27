4d6cb292885b91b0a69d22f64d95913ee517ab86
Helena Beranová <helca.beranova@gmail.com>
2024-06-27 20:15:42 +0200

    images changes

--- src/pages/about-us/EmployeeCard.tsx
+++ src/pages/about-us/EmployeeCard.tsx
@@ -1,5 +1,6 @@
 import { Container } from 'react-bootstrap';
 import profile1 from '../../assets/about-us/profile1.png';
+import helenka from '../../assets/about-us/helenka.jpg';
 import profile2 from '../../assets/about-us/profile2.png';
 import profile3 from '../../assets/about-us/profile3.png';
 import profile4 from '../../assets/about-us/profile4.png';
@@ -15,7 +16,7 @@ interface Employee {
 }
 
 export const EmployeeCard = ({ employee }: { employee: Employee }) => {
-  const profilePics = [profile1, profile2, profile3, profile4];
+  const profilePics = [profile1, helenka, profile3, profile4, profile2];
 
   return (
     <Container>
