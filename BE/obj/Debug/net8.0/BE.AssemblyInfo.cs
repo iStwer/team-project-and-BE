e9885220d461d621ccb5ef5cbc94103e1bb93ece
iStwer <istver99@gmail.com>
2024-07-24 16:07:30 +0200

    BookingForm.tsx rework

--- BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
+++ BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f80da2c4d106416ff3f761616c703ed445f5bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca75c2cf5dcf059c10c07593e3921fe7c6151d1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebUI/src/pages/booking-form/BookingForm.tsx
+++ WebUI/src/pages/booking-form/BookingForm.tsx
@@ -1,5 +1,5 @@
 import React, { FormEvent, ChangeEvent, useState, useEffect } from 'react';
-import { Container, Form, Button, Modal, FormControl, Row, Col } from 'react-bootstrap';
+import { Container, Form, Button, Modal } from 'react-bootstrap';
 import "react-datepicker/dist/react-datepicker.css";
 import './BookingForm.css';
 import services from '../../assets/services.json';
@@ -45,13 +45,12 @@ export const BookingForm = () => {
   const [formSubmitted, setFormSubmitted] = useState<boolean>(false);
   const [showModal, setShowModal] = useState<boolean>(false);
   const [availableHours, setAvailableHours] = useState([
-    '09:00','09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30',
+    '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30',
   ]);
 
   const [startDate, setStartDate] = useState<Date | null>(new Date());
   const [selectedDuration, setSelectedDuration] = useState<number | null>(null); // null, 30 or 60
   const [isEntranceExam, setIsEntranceExam] = useState<boolean>(false); // Track if service is "Vstupní vyšetření"
-
 
   const handleDateChange = (date: Date | null) => {
     setStartDate(date);
@@ -65,10 +64,13 @@ export const BookingForm = () => {
     return day !== 0 && day !== 6;
   };
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const target = e.target as HTMLInputElement | HTMLSelectElement;
+    const { name, value } = target;
+
     setInputData((prevData) => ({ ...prevData, [name]: value }));
-    if (value === 'Vstupní vyšetření') {
+
+    if (name === 'service' && value === 'Vstupní vyšetření') {
       setIsEntranceExam(true);
       setSelectedDuration(60); // Force duration to 60 min for Entrance examination
       setInputData((prevData) => ({ ...prevData, duration: '60' }));
@@ -82,16 +84,16 @@ export const BookingForm = () => {
       return;
     }
     setSelectedDuration(duration === selectedDuration ? null : duration);
-     // If 60 min duration is selected, adjust available hours to only hourly slots
-     if (duration === 60) {
+    // If 60 min duration is selected, adjust available hours to only hourly slots
+    if (duration === 60) {
       setAvailableHours([
         '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
       ]);
     } else {
       // Otherwise, reset available hours to default
       setAvailableHours([
-        '09:00','09:30', '10:00', '10:30', '11:00', '11:30', '12:00', 
-        '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', 
+        '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00',
+        '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30',
         '16:00', '16:30', '17:00', '17:30',
       ]);
     }
@@ -105,83 +107,58 @@ export const BookingForm = () => {
       setDurationError(true);
       return;
     }
-    await checkAvailabilityAndSubmit();
-    
+
+    // Check validity of data before sending to the server
+    let checkedTelephone = inputData.telephone.trim().replace(/\s+/g, '');
+    let checkedEmail = isValidEmail(inputData.email.trim());
+
+    if (isNaN(parseInt(checkedTelephone)) || checkedTelephone.length > 9) {
+      setTelephoneError(true);
+      setEmailError(false);
+      return;
+    } else if (!checkedEmail) {
+      setEmailError(true);
+      setTelephoneError(false);
+      return;
+    } else {
+      setTelephoneError(false);
+      setEmailError(false);
+    }
+
+    const formattedData = {
+      ...inputData,
+      duration: selectedDuration === 30 ? '30 min' : '60 min',
+    };
+
+    try {
+      const response = await axios.post('http://localhost:3001/api/bookings', formattedData);
+      console.log('Booking submitted', response.data);
+      localStorage.setItem('formSubmitted', 'true');
+      setShowModal(true);
+      setFormSubmitted(true);
+      // Reset form data
+      setInputData({
+        id: undefined,
+        name: '',
+        surname: '',
+        email: '',
+        telephone: '',
+        service: '',
+        date: '',
+        time: '',
+        duration: '',
+      });
+      setStartDate(null);
+      setSelectedDuration(null);
+    } catch (error) {
+      console.error('Error submitting the form:', error);
+    }
   };
 
   const isValidEmail = (email: string) => {
     const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return pattern.test(email);
   };
-
-  const checkAvailabilityAndSubmit = async () => {
-    try {
-      const response = await fetch('http://localhost:3001/bookings').then((res) => res.json());
-      let checkedTelephone = inputData.telephone.trim().replace(/\s+/g, '');
-      let checkedEmail = isValidEmail(inputData.email.trim());
-
-      if (isNaN(parseInt(checkedTelephone)) || checkedTelephone.length > 9) {
-        setTelephoneError(true);
-        setEmailError(false);
-      } else if (!checkedEmail) {
-        setEmailError(true);
-        setTelephoneError(false);
-      } else {
-        const formattedData = {
-          id: response[response.length - 1].id + 1,
-          name: inputData.name.trim(),
-          surname: inputData.surname.trim(),
-          email: inputData.email.trim(),
-          telephone: checkedTelephone,
-          service: inputData.service.trim(),
-          date: inputData.date.trim(),
-          time: inputData.time.trim(),
-          duration: selectedDuration === 30 ? '30 min' : '60 min',
-        };
-
-        setTelephoneError(false);
-        setEmailError(false);
-        setCheckedInputData(formattedData);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  useEffect(() => {
-    if (checkedInputData) {
-      const postNewBooking = async () => {
-        try {
-          if (checkedInputData) {
-            const response = await axios.post('http://localhost:3001/bookings', checkedInputData);
-            console.log('Booking submitted', response.data);
-            localStorage.setItem('formSubmitted', 'true');
-            setShowModal(true);
-            setFormSubmitted(true);
-          }
-        } catch (err) {
-          console.error(err);
-        } finally {
-          setCheckedInputData(null);
-          setInputData({
-            id: undefined,
-            name: '',
-            surname: '',
-            email: '',
-            telephone: '',
-            service: '',
-            date: '',
-            time: '',
-            duration: '',
-          });
-          setTelephoneError(false);
-          setEmailError(false);
-        }
-      };
-
-      postNewBooking();
-    }
-  }, [checkedInputData]);
 
   useEffect(() => {
     if (localStorage.getItem('formSubmitted') === 'true') {
@@ -195,7 +172,7 @@ export const BookingForm = () => {
 
   return (
     <Container className='mt-5 form-container'>
-      {localStorage.getItem('formSubmitted') && (
+      {formSubmitted && (
         <div className='info-message'>Formulář byl úspěšně odeslán!</div>
       )}
 
@@ -220,7 +197,7 @@ export const BookingForm = () => {
             id='name'
             name='name'
             value={inputData.name}
-            onChange={handleChange}
+            onChange={handleChange as React.ChangeEventHandler<HTMLInputElement>}
             required
           />
         </Form.Group>
@@ -233,7 +210,7 @@ export const BookingForm = () => {
             id='surname'
             name='surname'
             value={inputData.surname}
-            onChange={handleChange}
+            onChange={handleChange as React.ChangeEventHandler<HTMLInputElement>}
             required
           />
         </Form.Group>
@@ -246,114 +223,109 @@ export const BookingForm = () => {
             id='email'
             name='email'
             value={inputData.email}
-            onChange={handleChange}
+            onChange={handleChange as React.ChangeEventHandler<HTMLInputElement>}
+            isInvalid={emailError}
             required
           />
-
-          {emailError && (
-            <Form.Text>Zadejte platný e-mail!<br /></Form.Text>
-          )}
-
-          <Form.Text className='text-muted'>Váš e-mail nebudeme s nikým sdílet.</Form.Text>
+          <Form.Control.Feedback type='invalid'>
+            Zadejte platný e-mail.
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className='mb-3'>
           <Form.Label htmlFor='telephone'>Telefonní číslo</Form.Label>
           <Form.Control
-            type='tel'
+            type='text'
             placeholder='Telefonní číslo'
             id='telephone'
             name='telephone'
             value={inputData.telephone}
-            onChange={handleChange}
+            onChange={handleChange as React.ChangeEventHandler<HTMLInputElement>}
+            isInvalid={telephoneError}
             required
           />
-
-          {telephoneError && (
-            <Form.Text>Zadejte platné telefonní číslo! (pouze číslice, bez předvolby)<br /></Form.Text>
-          )}
-
-          <Form.Text className='text-muted'>Vaše telefonní číslo nebudeme s nikým sdílet.</Form.Text>
+          <Form.Control.Feedback type='invalid'>
+            Zadejte platné telefonní číslo (max. 9 číslic).
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className='mb-3'>
-          <Form.Label htmlFor='sluzby'>Vyberte službu:</Form.Label>
-          <Form.Control
-            as='select'
-            id='sluzby'
+          <Form.Label htmlFor='service'>Služba</Form.Label>
+          <Form.Select
+            id='service'
             name='service'
             value={inputData.service}
-            onChange={handleChange}
+            onChange={handleChange as React.ChangeEventHandler<HTMLSelectElement>}
             required
           >
-            <option value='' disabled hidden>--vyberte službu--</option>
+            <option value=''>Vyberte službu</option>
             {services.map((service) => (
-              <option key={service.id} value={service.name}>{service.name}</option>
+              <option key={service.name} value={service.name}>
+                {service.name}
+              </option>
             ))}
-          </Form.Control>
+          </Form.Select>
         </Form.Group>
 
         <Form.Group className='mb-3'>
-          <Form.Label>Vyberte délku trvání:</Form.Label>
-          <div>
-            <Button
-              onClick={() => handleDurationSelection(30)}
-              className={`custom-duration-button me-2 ${selectedDuration === 30 ? 'selected' : ''}`}
-              value = {inputData.duration} 
-              disabled={isEntranceExam} // Disable 30 min if Entrance examination selected
-            >
-              30 min
-            </Button>
-            <Button
-               className={`custom-duration-button me-2 ${selectedDuration === 60 ? 'selected' : ''}`}
-              onClick={() => handleDurationSelection(60)}
-              value = {inputData.duration}
-            >
-              60 min
-            </Button>
-          </div>
-          {durationError && (
-            <Form.Text className='text-muted'>Vyberte délku trvání!<br /></Form.Text>
-          )}
-        </Form.Group>
-
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='date'>Vyberte termín:</Form.Label>
-          <br></br>
-          <FormControl
-            as={DatePicker as any}
+          <Form.Label>Datum</Form.Label>
+          <DatePicker
             selected={startDate}
-            onChange={handleDateChange as any}
+            onChange={handleDateChange}
+            minDate={new Date(today)}
+            maxDate={new Date(maxDate)}
             filterDate={isWeekday}
-            minDate={new Date()}
-            dateFormat="dd-MM-yyyy"
-            id='date'
-            name='date'
+            dateFormat='dd/MM/yyyy'
             locale='cs'
-            value = {inputData.date}
+            className='form-control'
             required
           />
-        
         </Form.Group>
 
         <Form.Group className='mb-3'>
-          <Form.Label htmlFor='time'>Vyberte čas návštěvy:</Form.Label>
-          <Form.Control
-            as='select'
+          <Form.Label htmlFor='time'>Čas</Form.Label>
+          <Form.Select
             id='time'
             name='time'
             value={inputData.time}
-            onChange={handleChange}
+            onChange={handleChange as React.ChangeEventHandler<HTMLSelectElement>}
             required
           >
-            <option value='' disabled hidden>Vyberte čas návštěvy</option>
+            <option value=''>Vyberte čas</option>
             {availableHours.map((hour) => (
-              <option key={hour} value={hour}>{hour}</option>
+              <option key={hour} value={hour}>
+                {hour}
+              </option>
             ))}
-          </Form.Control>
+          </Form.Select>
         </Form.Group>
 
-        <Button type='submit' className='action-btn mt-3 mb-5'>Objednat se</Button>
+        <Form.Group className='mb-3'>
+          <Form.Label>Trvání</Form.Label>
+          <div className='duration-options'>
+            <Form.Check
+              type='radio'
+              label='30 minut'
+              name='duration'
+              checked={selectedDuration === 30}
+              onChange={() => handleDurationSelection(30)}
+            />
+            <Form.Check
+              type='radio'
+              label='60 minut'
+              name='duration'
+              checked={selectedDuration === 60}
+              onChange={() => handleDurationSelection(60)}
+            />
+          </div>
+          {durationError && (
+            <div className='text-danger'>Vyberte trvání služby.</div>
+          )}
+        </Form.Group>
+
+        <Button variant='primary' type='submit'>
+          Odeslat
+        </Button>
       </Form>
     </Container>
   );
