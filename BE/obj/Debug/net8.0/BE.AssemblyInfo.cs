1b72e93e3b58954da5652d8aaa769797cca66aa5
iStwer <istver99@gmail.com>
2024-07-24 12:55:09 +0200

    tt

--- BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
+++ BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e13d54193353a8011e5fc6acf8521adbcfeda8ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d557b4f4b849bf306386905884eeeacbf31eb70d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
