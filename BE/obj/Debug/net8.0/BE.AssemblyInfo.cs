61f80da2c4d106416ff3f761616c703ed445f5bd
iStwer <istver99@gmail.com>
2024-07-24 13:09:47 +0200

    Random

--- BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
+++ BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f23b81786092368de31cfe9eb3b32c34a6c12e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b697feeecda5ea2607586d02e877d07114e125")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
